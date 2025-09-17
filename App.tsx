cf5e9011085a41c96e7becd1c78ec48d0a871996
ThiagoJO0705 <rm551624@fiap.com.br>
2025-09-17 16:52:48 -0300

    Adcionando a pasta src e colcoando os arquivos nela

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ActivityIndicator, View } from "react-native";
-import { AuthProvider, useAuth } from "./context/AuthContext";
-import MainNavigator from "./navigation/TabNavigator";
-import AuthNavigator from "./navigation/AuthNavigator";
+import { AuthProvider, useAuth } from "./src/context/AuthContext";
+import MainNavigator from "./src/navigation/TabNavigator";
+import AuthNavigator from "./src/navigation/AuthNavigator";
 
 const AppContent = () => {
   const { user, isLoading } = useAuth();
